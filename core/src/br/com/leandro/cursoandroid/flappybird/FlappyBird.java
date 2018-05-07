edf1e4f5b0f592df8d517d6af1ccaca65f4984e1
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-07 18:07:47 -0400

    Segunda parte.

--- core/src/br/com/leandro/cursoandroid/flappybird/FlappyBird.java
+++ core/src/br/com/leandro/cursoandroid/flappybird/FlappyBird.java
@@ -2,34 +2,45 @@ package br.com.leandro.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import javax.xml.soap.Text;
 
 public class FlappyBird extends ApplicationAdapter {
 
     private SpriteBatch batch;
     private Texture passaro;
+    private int movimento = 0;
 
-	@Override
-	public void create () {
+    //Atributos de configuração
+    private Texture fundo;
+    private int larguraDispositivo;
+    private int alturaDispositivo;
+
+
+    @Override
+    public void create() {
 
         batch = new SpriteBatch();
         passaro = new Texture("passaro1.png");
-	}
+        fundo = new Texture("fundo.png");
 
-	@Override
-	public void render () {
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
+
+    }
+
+    @Override
+    public void render() {
+        movimento += 4;
 
         batch.begin();
 
-        batch.draw(passaro, 300,300);
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(passaro, movimento, 400);
 
         batch.end();
 
-	}
-	
+    }
+
 
 }
