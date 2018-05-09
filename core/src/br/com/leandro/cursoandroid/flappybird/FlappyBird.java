2c50a3d026b8858e3f0801756e947927d25ffc63
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-09 09:51:04 -0400

    Pontuação.

--- core/src/br/com/leandro/cursoandroid/flappybird/FlappyBird.java
+++ core/src/br/com/leandro/cursoandroid/flappybird/FlappyBird.java
@@ -2,7 +2,9 @@ package br.com.leandro.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -15,11 +17,14 @@ public class FlappyBird extends ApplicationAdapter {
     private Texture canoBaixo;
     private Texture canoTopo;
     private Random numeroRandomico;
+    private BitmapFont fonte;
 
     //Atributos de configuração
     private Texture fundo;
     private int larguraDispositivo;
     private int alturaDispositivo;
+    private int estadoJogo = 0;//0 -> jogo não iniciado e 1 -> jogo iniciado
+    private int pontuacao = 0;
 
     private float variacao = 0;
     private float velocidadeQueda = 0;
@@ -28,6 +33,7 @@ public class FlappyBird extends ApplicationAdapter {
     private float espacoEntreCanos;
     private float deltaTime;
     private float alturaEntreCanosRandomica;
+    private boolean marcouPonto;
 
 
     @Override
@@ -35,6 +41,9 @@ public class FlappyBird extends ApplicationAdapter {
 
         batch = new SpriteBatch();
         numeroRandomico = new Random();
+        fonte = new BitmapFont();
+        fonte.setColor(Color.WHITE);
+        fonte.getData().setScale(6);
 
         passaros = new Texture[3];
         passaros[0] = new Texture("passaro1.png");
@@ -58,25 +67,41 @@ public class FlappyBird extends ApplicationAdapter {
 
         deltaTime = Gdx.graphics.getDeltaTime();
         variacao += deltaTime * 10;
-        posicaoMovimentoCanoHorizontal -= deltaTime * 200;
-        velocidadeQueda++;
-        if (variacao > 2) {
-            variacao = 0;
+        if (variacao > 2) variacao = 0;
+
+
+        if (estadoJogo == 0) {//Não iniciado
+            if (Gdx.input.justTouched()) {
+                estadoJogo = 1;
+            }
+        } else {
+
+            posicaoMovimentoCanoHorizontal -= deltaTime * 200;
+            velocidadeQueda++;
+
+
+            if (Gdx.input.justTouched()) {
+                velocidadeQueda = -15;
+            }
+
+            if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
+                posicaoInicialVertical -= velocidadeQueda;
+
+            // Verifica se o cano saiu inteiramente da tela
+            if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
+                posicaoMovimentoCanoHorizontal = larguraDispositivo;
+                alturaEntreCanosRandomica = numeroRandomico.nextInt((400) - 200);
+                marcouPonto = false;
+            }
+
+            //Verifica pontuação
+            if(posicaoMovimentoCanoHorizontal < 120) {
+                if( !marcouPonto ) {
+                    pontuacao++;
+                    marcouPonto = true;
+                }
+            }
         }
-
-        if (Gdx.input.justTouched()) {
-            velocidadeQueda = -15;
-        }
-
-        if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
-            posicaoInicialVertical -= velocidadeQueda;
-
-        // Verifica se o cano saiu inteiramente da tela
-        if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
-            posicaoMovimentoCanoHorizontal = larguraDispositivo;
-            alturaEntreCanosRandomica = numeroRandomico.nextInt((400) - 200);
-        }
-
         batch.begin();
 
         batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
@@ -85,6 +110,8 @@ public class FlappyBird extends ApplicationAdapter {
         batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2
                 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
         batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
+
+        fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo / 2, alturaDispositivo - 50);
 
         batch.end();
 
